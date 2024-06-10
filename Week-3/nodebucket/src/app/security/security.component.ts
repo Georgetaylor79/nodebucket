26150692c4194a893e7e048dde6a04286e6458eb
George <george_rtaylor@yahoo.com>
2024-06-10 18:32:27 -0500

    commit

---
+++ Week-3/nodebucket/src/app/security/security.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Title: security.component.ts
+ * Author: Professor Krasso
+ * Date: 8/5/23
+ */
+
+// imports statements
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-security',
+  // router outlet for the security module
+  template: `
+<router-outlet></router-outlet>
+  `,
+  styles: [
+  ]
+})
+export class SecurityComponent {
+
+}
