792bb991a9613514ed6cd6a881b01753382ce123
George <george_rtaylor@yahoo.com>
2024-06-10 19:53:59 -0500

    commit

--- Week-3/nodebucket/server/routes/employee-route.js
+++ Week-3/nodebucket/server/routes/employee-route.js
@@ -24,20 +24,60 @@ router.get("/:empId", (req, res, next) => {
       return next(createError(400, "Input must be a number"));
     }
 
+
     // database query is handled
     mongo(async db => {
-      const employee = await db.collection("employees").findOne({ empId });
+      const employee = await db.collection("employees").findOne({ empId }); // find the  employee by empId
 
       if (!employee) {
+        // If the employee is not found
         console.error("Employee not found", empId);
         return next(createError(404, "Employee not found"));
       }
-      res.send(employee);
+      res.send(employee); // return the employee object
     }, next)
   } catch (err) {
     console.error("Error:", err);
+    next(err); // forward the error to the error handler
+  }
+})
+
+/**
+ * findAllTask API
+ * @returns JSON array of all tasks
+ * @throws { 500 error } - if there is a server error
+ * @throws { 400 error } - if the employee id is not a number
+ * @throws { 404 error } - if no task are found
+ */
+router.get('/:empId/tasks', (req, res, next) => {
+  try{
+
+    let { empId } = req.params;
+    empId = parseInt(empId, 10);
+
+    // Check to determine if the returned value from parseInt is Nan
+    if (isNaN(empId)) {
+      return next(createError(400, 'Employee ID must be a number'));
+    }
+
+    //Call our mongo mongo module and return a list of tasks by employee ID
+    mongo(async db => {
+
+      const tasks = await db.collection('employee').findOne( { empId: empId}, { projection: { empId: 1, todo: 1, done: 1} } );
+
+      console.log('task', task);
+
+      // If there are no tasks found for the employee Id; return a 404 error object to our middleware error handler
+      if(!tasks) {
+        return next(createError(404, `Tasks not found for employee ID ${empId} `));
+      }
+
+        res.send(tasks);
+    }, next);
+
+  } catch(err) {
+    console.error('err',err);
     next(err);
   }
 });
-
-    module.exports = router;
+    module.exports = router; // end module.exports = router
