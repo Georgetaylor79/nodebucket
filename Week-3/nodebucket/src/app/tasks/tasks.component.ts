eb56986e732bac1376e3752344b7d5a4765a05a5
George <george_rtaylor@yahoo.com>
2024-06-12 20:02:22 -0500

    commit

--- Week-3/nodebucket/src/app/tasks/tasks.component.ts
+++ Week-3/nodebucket/src/app/tasks/tasks.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpClient } from '@angular/common/http';
+
+export interface Item {
+  _id: string;
+  text: string;
+}
+
+export interface Employee {
+  empId: number;
+  todo: Item[];
+  done: Item[];
+}
 
 @Component({
   selector: 'app-tasks',
@@ -6,5 +20,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
+  //Local variables
+  empId: number;
+  employee: Employee;
+  todo: Item[];
+  done: Item[];
+
+  constructor(private http: HttpClient, private cookieService: CookieService) {
+    this.empId = parseInt(this.cookieService.get('session_user'), 10);
+    this.employee= {} as Employee;
+    this.todo =[];
+    this.done = [];
+  }
+
 
 }
